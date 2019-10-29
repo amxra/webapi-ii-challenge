28b018b6feaa9ff5d2ad040d3cd247b125f3a11e
amxra <amiradediran@gmail.com>
2019-10-29 21:12:51 +0100

    listen on process.env.PORT || 3000

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const express = require('express');
 const server = express()
 
 server.use(express.json())
+
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('listening on port ' + (process.env.PORT || 3000))
+})
