084bb779126e7438e119c326772ff72b6e4e87dc
amxra <amiradediran@gmail.com>
2019-10-29 21:17:49 +0100

    Import database into index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const server = express()
 
 server.use(express.json())
 
+const db = require('./data/db')
+
+
+
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('listening on port ' + (process.env.PORT || 3000))
